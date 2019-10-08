c5681d1c43e443506a25d0d485e632e007dfb1e6
mellenwo <mellenwo@gmail.com>
2019-10-07 20:57:15 -0700

    first commit

---
+++ app/src/main/java/com/mellenwood/parsable/ItemListActivity.kt
@@ -0,0 +1,133 @@
+package com.mellenwood.parsable
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+import com.mellenwood.parsable.dummy.DummyContent
+import kotlinx.android.synthetic.main.activity_item_list.*
+import kotlinx.android.synthetic.main.item_list_content.view.*
+import kotlinx.android.synthetic.main.item_list.*
+import kotlinx.coroutines.*
+
+/**
+ * An activity representing a list of Pings. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a [ItemDetailActivity] representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+class ItemListActivity : AppCompatActivity() {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private var twoPane: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_list)
+
+        setSupportActionBar(toolbar)
+        toolbar.title = title
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        if (item_detail_container != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            twoPane = true
+        }
+
+        setupRecyclerView(item_list)
+    }
+
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, twoPane)
+    }
+
+    class SimpleItemRecyclerViewAdapter(
+        private val parentActivity: ItemListActivity,
+        private val values: MutableList<DummyContent.DummyItem>,
+        private val twoPane: Boolean
+    ) :
+        RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
+
+        private val onClickListener: View.OnClickListener
+
+        init {
+            onClickListener = View.OnClickListener { v ->
+                val item = v.tag as DummyContent.DummyItem
+                updateData(item)
+            }
+        }
+
+        private fun updateData(item: DummyContent.DummyItem) {
+            runBlocking {
+                if (item.result == null) {
+
+                    val newNum = async {
+                        Math.pow(
+                            item.id.toDouble(),
+                            4.toDouble()
+                        )
+                    }
+                    values[item.id.toInt() - 1] =
+                        DummyContent.DummyItem(
+                            item.id,
+                            "Item " + item.id,
+                            newNum.await().toInt().toString()
+                        )
+                } else {
+                    values[item.id.toInt() - 1] =
+                        DummyContent.DummyItem(
+                            item.id,
+                            "Item " + item.id,
+                            null
+                        )
+                }
+                notifyDataSetChanged()
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_list_content, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val item = values[position]
+            holder.idView.text = item.id
+            holder.contentView.text = item.content
+            holder.result.text = item.result
+
+
+            with(holder.itemView) {
+                tag = item
+                setOnClickListener(onClickListener)
+            }
+        }
+
+        override fun getItemCount() = values.size
+
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val idView: TextView = view.id_text
+            val contentView: TextView = view.content
+            val result: TextView = view.result_text
+        }
+    }
+}
